8f212324eb8de36d0d6ccedc928fa3c903cb47e5
XavierOndevilla <ondevillaxavier@gmail.com>
2020-12-18 00:30:37 -0500

    Added message
    -Changed header in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>The site is now live!</h1>
+        <h1>The site is currently under construction!</h1>
       </header>
     </div>
   );

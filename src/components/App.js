4501cc4137035bf7bec190f8484a87f3b7bcfb38
XavierOndevilla <ondevillaxavier@gmail.com>
2021-01-11 20:29:43 -0500

    Changed colors and text
    - Changed text from "The site is currently under construction" to "X.O"
    - Changed font size of header from 10px + 2 vmin to 50px + 2vmin
    - Changed background color to #040f0f
    - Changed text color to #f5fbef

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>The site is currently under construction!</h1>
+        <h1>X.O</h1>
       </header>
     </div>
   );

f930c53fbda399fc72fa01ab3f2c132a63f726d8
XavierOndevilla <ondevillaxavier@gmail.com>
2022-01-23 13:15:20 -0500

    Created About section
    -Added new directory "/about"

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './header/header.js';
+import About from './about/about.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/about.js
@@ -0,0 +1,16 @@
+import './about.css';
+
+export default function About() {
+    return (
+        <div className="About">
+            <h1 className='About-header'>About</h1>
+            <p className="About-text">
+                &emsp;Hey there! My name is Xavier Ondevilla. Welcome to my website! Here, you can learn some little things about me and what I do for fun.
+            </p>
+            <p className="About-text">
+                &emsp;I am a web developer with experience using vanilla JavaScript, jQuery, SASS, NodeJS, ReactJS and some Angular. I graduated from Ryerson University with a BSc in Computer Science and did a few co-ops with Ministry of Health and Long-Term Care
+                and Environment and Climate Change Canada. Right now you can catch me at Rich Media.
+            </p>
+        </div>
+    );
+}

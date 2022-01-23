da13657015465e798ef8aa7f5d06266cc86e1e5b
XavierOndevilla <ondevillaxavier@gmail.com>
2022-01-23 14:44:14 -0500

    Update "About Me" section
    - Added profile picture
    - Added header
    - Added text

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,16 +1,26 @@
 import './about.css';
+import Profile from "../../assets/images/profile.jpg"
 
 export default function About() {
     return (
         <div className="About">
-            <h1 className='About-header'>About</h1>
-            <p className="About-text">
-                &emsp;Hey there! My name is Xavier Ondevilla. Welcome to my website! Here, you can learn some little things about me and what I do for fun.
-            </p>
-            <p className="About-text">
-                &emsp;I am a web developer with experience using vanilla JavaScript, jQuery, SASS, NodeJS, ReactJS and some Angular. I graduated from Ryerson University with a BSc in Computer Science and did a few co-ops with Ministry of Health and Long-Term Care
-                and Environment and Climate Change Canada. Right now you can catch me at Rich Media.
-            </p>
+            <div className='About-left'>
+                <h2>About Me</h2>
+                <p className="About-text">
+                    Hey there! My name is Xavier Ondevilla and I'm a 24-year-old full stack web developer based in the GTA.
+                </p>
+                <p className="About-text">
+                    I graduated with Honours from Ryerson University with a BSc. in Computer Science and a concentration in software engineering. In a nutshell, I've worked with a lot of different languages and tools such as JavaScript, jQuery, React, NodeJS,
+                    PHP, SQL and so on.
+                </p>
+                <p className="About-text">
+                    When I'm not coding, I like to write a lot (and I mean a lot) of music. I'm also a hardcore audiophile; I'm always looking for the best speakers and headphones, despite my wallet's desperate pleas for help.
+                </p>
+            </div>
+            <div className='About-right'>
+                <img src={Profile} alt="Profile"></img>
+            </div>
+            
         </div>
     );
 }

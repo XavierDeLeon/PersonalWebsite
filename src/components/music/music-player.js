315e20567b4bd48c4266ec36dec8907ef0cda0bd
XavierOndevilla <ondevillaxavier@gmail.com>
2022-01-24 03:08:22 -0500

    Layout Change
    - Switched to horizontal scroll layout
    - Added navigation
    - Added player functionality

--- src/components/music/music-player.js
+++ src/components/music/music-player.js
@@ -1,18 +1,61 @@
 import "./music-player.css";
+import { useRef, useEffect, useState } from "react";
+
+// A Place to Call Home
 import aPlaceToCallHomeUrl from "../../assets/audio/A-Place-to-Call-Home.mp3"
 import aPlaceToCallHomeImgUrl from "../../assets/images/A-Place-to-Call-Home.jpg"
+// Comet
 import cometUrl from "../../assets/audio/Comet.mp3"
 import cometImgUrl from "../../assets/images/Comet.jpg"
+// Smile
+import smileUrl from "../../assets/audio/Smile.mp3"
+import smileImgUrl from "../../assets/images/Smile.jpg"
 
 export default function MusicPlayer() {
 
     // music navigation vars
-    let currentSong = 0;
-    const musicMap = new Map();
-    const musicNav = ["A Place to Call Home", "Comet"];
+    const [songIndex, updateSongIndex] = useState(0);
+    const songArr = [ //important: tracklist goes in order
+        {
+            "title": "A Place to Call Home", 
+            "audio": aPlaceToCallHomeUrl, 
+            "image": aPlaceToCallHomeImgUrl,
+            "about": "The reason I wrote this song was actually pretty cheesy. My girlfriend went to South Korea to study abroad and since she was feeling nervous, I wanted to make a song that would make her feel better. Unfortunately, I was only actually able to complete this song well after she got back. Nonetheless I put all the feelings I had back then and put them into this song."
+        },
+        {
+            "title": "Comet",
+            "audio": cometUrl, 
+            "image": cometImgUrl,
+            "about": "This song was written when I was thinking about past experiences with friends and how we've all grown up since then. It's a bittersweet song about those fleeting moments and how much you should cherish them."
+        },
+        {
+            "title": "Smile",
+            "audio": smileUrl, 
+            "image": smileImgUrl,
+            "about": "I actually wrote this one to cheer myself up. Things weren't working out as I hoped and I got into a little bit of a slump. I watched Kiki's Delivery Service and something about that inspired me to make this song to remind myself to smile. Hence the sound clips and album art."
+        },
+    ];
 
-    musicMap.set("A Place to Call Home", {"audio": aPlaceToCallHomeUrl, "image": aPlaceToCallHomeImgUrl});
-    musicMap.set("Comet", {"audio": cometUrl, "image": cometImgUrl});
+    const playerElem = useRef(null);
+    const [height, setHeight] = useState(0);
+    let firstRender = false;
+
+    useEffect(() => {
+        let handleResize = () => {
+            setHeight(playerElem.current.clientWidth);
+        }
+
+        if (!firstRender) {
+            setHeight(playerElem.current.clientWidth);
+            firstRender = true;
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    });
 
     const playOrStop = () => {
         if (document.getElementById("src").paused){
@@ -22,23 +65,37 @@ export default function MusicPlayer() {
         }
     }
 
-    const changeMusic = () => {
-        currentSong = currentSong + 1 < musicNav.length ? currentSong + 1 : 0;
-        document.getElementById("src").src = musicMap.get(musicNav[currentSong])["audio"];
-        document.getElementById("player").style.backgroundImage = `url(${musicMap.get(musicNav[currentSong])["image"]})`;
-        document.getElementById("music-title").textContent = musicNav[currentSong];
+    const changeSong = () => {
+        if (songIndex + 1 < songArr.length) {
+            updateSongIndex(songIndex + 1);
+        } else {
+            updateSongIndex(0);
+        }
     }
 
     return (
-        <>
-            <h2 id="music-title">A Place to Call Home</h2>
+        <div className="music-container">
             <div className="music-player">
-                <div className="left-arrow" onClick={changeMusic}>&#x2190;</div>
-                <div id="player" className="player" onClick={playOrStop}>
-                    <audio id="src" src={aPlaceToCallHomeUrl} controls/>
+                <div className="left-arrow" onClick={changeSong}>&#x2190;</div>
+                <div ref={playerElem} id="player" 
+                     className="player" 
+                     style={{
+                         backgroundImage: `url(${songArr[songIndex]["image"]})`,
+                         backgroundSize: "contain",
+                         height: `${height}px`
+                     }} 
+                     onClick={playOrStop}
+                >
+                    <audio id="src" src={songArr[songIndex]["audio"]} controls/>
                 </div>
-                <div className="right-arrow" onClick={changeMusic}>&#x2192;</div>
+                <div className="right-arrow" onClick={changeSong}>&#x2192;</div>
             </div>
-        </>
+            <div className="music-player-details">
+                <h2 id="music-title">{songArr[songIndex]["title"]}</h2>
+                <hr></hr>
+                <h3>About this song</h3>
+                <p>&emsp;{songArr[songIndex]["about"]}</p>
+            </div>
+        </div>
     );
 }
--- src/components/music/music.js
+++ src/components/music/music.js
@@ -4,6 +4,7 @@ import MusicPlayer from "./music-player";
 export default function MusicSection() {
     return (
         <div className="music-body">
+            <h2>My Music</h2>
             <div className="music-player-container">
                 <MusicPlayer />
             </div>
